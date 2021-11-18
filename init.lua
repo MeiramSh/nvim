18454d09874f39c1c2c9f3a4e0c79bbd6bf14e17
Meiram Shunshalin <meiram.shunshalin@nu.edu.kz>
2021-11-19 02:33:19 +0600

    Added packer and autopair

--- init.lua
+++ init.lua
@@ -1,3 +1,19 @@
+cmd = vim.api.nvim_command
+join_paths = require 'packer.util'.join_paths
+
+cmd ('set packpath+='..join_paths(vim.fn.stdpath('config'), 'plugins', 'packer'))
+cmd ('highlight MatchParen ctermfg=7 ctermbg=0')
+
+-- packer
+local packer = require 'packer'
+local use = packer.use
+
 packer.init({
-	package_root = util.join_paths(vim.fn.config('data'), 'packer', 'packeges'),	
-}
+	package_root = join_paths(vim.fn.stdpath('config'), 'plugins'),	
+})
+
+use 'wbthomason/packer.nvim'
+use 'windwp/nvim-autopairs'
+
+-- autopairs
+require('nvim-autopairs').setup{}
