a7eeb25f7eaa29fc5f34c10ff743443b7226b3d9
Meiram Shunshalin <meiram.shunshalin@nu.edu.kz>
2023-04-14 21:34:49 +0600

    Initial commit

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- options
+vim.g.mapleader = ' '
+vim.o.shell = '/bin/zsh'
+vim.o.expandtab = true -- space instead of tabs
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+vim.o.number = true
+vim.o.relativenumber = true
+vim.o.cursorline = true
+vim.opt.mouse = 'a'
+vim.opt.hidden = true
