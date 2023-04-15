9b122379e23aadd2ea45d1bd816046bbceaf445e
Meiram Shunshalin <meiram.shunshalin@nu.edu.kz>
2023-04-16 01:03:23 +0600

    Git signs

--- init.lua
+++ init.lua
@@ -7,8 +7,10 @@ vim.o.shiftwidth = 2
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.cursorline = true
+vim.o.spell = true
 vim.opt.mouse = 'a'
 vim.opt.hidden = true
+vim.opt.laststatus = 3
 
 
 
@@ -34,6 +36,52 @@ meiram       = b
 vim.opt.rtp:prepend(lazypath)
 
 require 'lazy'.setup {
+  {
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs                        = {
+        add          = { text = '│' },
+        change       = { text = '│' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~' },
+        untracked    = { text = '┆' },
+      },
+      signcolumn                   = true,  -- Toggle with `:Gitsigns toggle_signs`
+      numhl                        = false, -- Toggle with `:Gitsigns toggle_numhl`
+      linehl                       = false, -- Toggle with `:Gitsigns toggle_linehl`
+      word_diff                    = false, -- Toggle with `:Gitsigns toggle_word_diff`
+      watch_gitdir                 = {
+        interval = 1000,
+        follow_files = true,
+      },
+      attach_to_untracked          = true,
+      current_line_blame           = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts      = {
+        virt_text = true,
+        virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+      },
+      current_line_blame_formatter = '<author>, <author_time:%Y-%m-%d> - <summary>',
+      sign_priority                = 6,
+      update_debounce              = 100,
+      status_formatter             = nil,   -- Use default
+      max_file_length              = 40000, -- Disable if file is longer than this (in lines)
+      preview_config               = {
+        -- Options passed to nvim_open_win
+        border = 'single',
+        style = 'minimal',
+        relative = 'cursor',
+        row = 0,
+        col = 1,
+      },
+      yadm                         = {
+        enable = false,
+      },
+    },
+  },
+
   {
     'folke/noice.nvim',
     dependencies = {
